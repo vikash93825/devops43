95f41847a60c7c3b0b9adcd41d9eae2598c36ca1
Vikash Kumar <vikashkumar.kk@gmail.com>
2026-04-23 09:40:35 +0530

    Modify HelloWorld class to print new greetings
    Updated the main method to print new greeting messages.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-public class Simple {
-    public static void main(String[] args) {
-        System.out.println("Learning Devops from Vikash Ratnawat and 43 students");
-    }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch43 team with  Mohit, Yogesh , Sachoin");
+      System.out.println("Hello Batch batch42 WITH FOR STUDENTS RUNNING in front of me and Anurag Prince in making CI automated vikas teach devops CI proecss");
+    }  
+} 
